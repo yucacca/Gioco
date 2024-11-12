16a4cdeaf4a6a7f2f840fceeb7bf4f3de0a58fbe
yuki <yukicolombo@gmail.com>
2024-11-12 10:37:41 +0100

    Funzioni livello 1 e player

---
+++ src/player.js
@@ -0,0 +1,11 @@
+function preload_player(s) {
+    
+}
+
+function create_player(s) {
+    
+}
+
+function update_player(s) {
+    
+}
--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -1,13 +1,13 @@
 function preload (s) {
-
+    preload_player(s);
 }
 
 function create (s){
-
+    create_player(s);
 }
 
 function update (s){
-
+    update_player(s);
 }
 
 function destroy (s)  {
