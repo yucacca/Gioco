8856c842cceab2ad85541755dbc4dd03949c9444
yuki <yukicolombo@gmail.com>
2024-11-12 10:30:07 +0100

    Main e level1

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+let config={
+    canvas_width: 1280,
+    canvas_height: 720,
+    canvas_id: "game_area",
+    background_color: 0x00FFFF,
+    debug_mode: true, 
+    gravity_value:200
+};
+
+PP.game.create(config);
---
+++ src/scenes/level1.js
@@ -0,0 +1,17 @@
+function preload (s) {
+
+}
+
+function create (s){
+
+}
+
+function update (s){
+
+}
+
+function destroy (s)  {
+
+}
+
+PP.scenes.add ("level1", preload, create, update, destroy)
