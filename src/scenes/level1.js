a912258c8c2082299f0d4a833723a94d99917ec0
yucacca <yukicolombo@gmail.com>
2024-11-17 22:14:37 +0000

    passaggio da menù a livello 1

--- src/scenes/level1.js
+++ src/scenes/level1.js
@@ -1,9 +1,13 @@
+let img_background
+
 function preload (s) {
     preload_player(s);
+    img_background = PP.assets.image.load(s, "assets/images/eggdog.png");
 }
 
 function create (s){
     create_player(s);
+    PP.assets.image.add(s, img_background, 0 , 500, 0, 0)
 }
 
 function update (s){
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,16 +1,18 @@
 let img_background
+let gioca
 
 function preload (s) {
-    img_background = PP.assets.image.load(s, "assets/eggdog.png");
+    img_background = PP.assets.image.load(s, "assets/images/eggdog.png");
 }
 
 function create (s){
      PP.assets.image.add(s, img_background, 0 , 0, 0, 0)
      
+
      PP.shapes.text_styled_add(s, 
         PP.game.config.canvas_width / 2,
         200,
-        "L'anello magico",
+        "L'ANELLO MAGICO",
         100,
         "Helvetica",
         "normal",
@@ -19,8 +21,51 @@ function create (s){
         0.5,
         0.5);
 
+    PP.shapes.text_styled_add(s, 
+        100,
+        400,
+        "Storia",
+        75,
+        "Helvetica",
+        "normal",
+        "0xFFFFFF",
+        null,
+        0,
+        0.5);
+    
+    gioca = PP.shapes.text_styled_add(s, 
+        100,
+        500,
+        "Gioca",
+        75,
+        "Helvetica",
+        "normal",
+        "0xFFFFFF",
+        null,
+        0,
+        0.5);
 
-   
+     if (PP.interactive.mouse.add(gioca, "pointerdown",change_scene)){
+       
+      }
+
+    
+    PP.shapes.text_styled_add(s, 
+        100,
+        600,
+        "Crediti",
+        75,
+        "Helvetica",
+        "normal",
+        "0xFFFFFF",
+        null,
+        0,
+        0.5);
+
+    
+   function change_scene (s){
+    PP.scenes.start("level1");
+   }
 }
 
 function update (s){
@@ -30,5 +75,7 @@ function update (s){
 function destroy (s)  {
 
 }
+
+
 
 PP.scenes.add ("menu", preload, create, update, destroy)
